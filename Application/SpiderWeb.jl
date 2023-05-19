45a6c740898b2880219a4b8f8a421b6a61b8fa5b
Nils Niggemann <nils.niggemann@fu-berlin.de>
2023-05-19 09:28:51 +0200

    first implement of random gen. Does not work yet

--- Application/SpiderWeb.jl
+++ Application/SpiderWeb.jl
@@ -25,14 +25,22 @@ end
 
 ##
 using CairoMakie.Makie.ColorSchemes
-function plotSpinConfig!(ax,S;kwargs...)
-    hm = heatmap!(ax,Array(S.Mat),colorrange = (-S.S,S.S),color = :RdBu_11)
-    Color
+function plotSpinConfig!(ax,S::SW.SpinConfig;kwargs...)
+    vals = filter!(!ismissing,unique(S.Mat))
+
+    Amin = min(minimum(vals),-S.S)
+    Amax = max(maximum(vals),S.S)
+    us = Amin:Amax
+    hm = heatmap!(ax,Array(S.Mat),colorrange = (Amin,Amax),colormap = cgrad(:linear_bgy_10_95_c74_n256, length(us), categorical = true))
+    return hm
 end
+
 function plotSpinConfig(S;kwargs...) 
     fig = Figure()
-    ax = Axis(fig[1,1],aspect = 1,backgroundcolor = :grey)
-    plotSpinConfig!(ax,S;kwargs...)
+    ax = Axis(fig[1,1],aspect = DataAspect(),backgroundcolor = :grey)
+    hm = plotSpinConfig!(ax,S;kwargs...)
+    us = filter!(!ismissing,unique(S))
+    Colorbar(fig[1,2],hm,ticks = us)
     return fig
 end
 function drawPlaquette!(ax,(i,j);kwargs...)
@@ -81,45 +89,132 @@ function getStairCase(L)
 
 end
 
-function getApplicablePlaquettes(SpinConfig)
-    plaqPos = [(i,j) for i in axes(SpinConfig.Mat,1) for j in axes(SpinConfig.Mat,2) if SW.CanApplyPlaquette(SpinConfig,i,j)]
-    return plaqPos
+##
+function findOps(Conf)
+    r = -1.:1.
+    Allops = [SW.SpinConfig(SMatrix{3,3}(a,b,c,d,e,f,g,h,i),1/2) for a in r for b in r for c in r for d in r for e in r for f in r for g in r for h in r for i in r]
+    filter!(x ->SW.CanApplyAnywhere(Conf,x),Allops)
+    return Allops
 end
+##
 
 let 
-    StairCase = getStairCase(30)
+
+    StairCase = getStairCase(15)
+    b = findOps(StairCase)
+    plaqs = SW.getApplicablePlaquettes(StairCase,b[1])
     fig = plotSpinConfig(StairCase,markersize = 23)
     ax = current_axis()
-    plaqs = getApplicablePlaquettes(StairCase)
-    return plaqs
     points = Point2f.(plaqs)
     scatter!(ax,points,markersize = 13,color = :red)
     fig
 end
 ##
-let 
-    StairCase = getStairCase(30)
-    plaqs = getApplicablePlaquettes(StairCase)
-    for R in plaqs
-        SW.PlaquetteOperatorSave!(StairCase,R)
-        break
-    end
-    plotSpinConfig(StairCase,markersize = 23)
-end
-##
-
-function generateRandomGroundState(L,S=1/2;maxiter = 10000)
+function generateRandomGroundState(L,S=1/2;maxiter = 1_000_000)
     AllowedPlaquettes = SW.getAllGS(S)
-    Mat = rand(eachindex(AllowedPlaquettes),L,L)
-    SC = SW.SpinConfig(Mat,S)
-    sites_i = rand(1:L,maxiter)
-    sites_j = rand(1:L,maxiter)
 
-    for (i,j) in zip(sites_i,sites_j)
-        P = SW.getPlaquette(SC,i,j)
-        P .= -P
-        SW.fulFillsConstraint(SC) && return SC
+    PlaqetteMatrix = zeros(Int64,L,L)
+
+    randBuffer = rand(1:70,maxiter)
+    it = 0
+    for i in 1:L
+        for j in 1:L
+            PlaqetteMatrix[i,j] = randBuffer[(i-1)*L+j]
+            it +=1
+        end
     end
     return nothing
     error("Could not find a ground state")
 end
+generateRandomGroundState(10)
+##
+S1 = SW.constructSpinConfigFromPlaquettes(4,4,AllAllowedConfigs)
+##
+using FRGLatticeEvaluation
+
+##
+using FFTW
+
+"""given a matrix S_ij return the correlator C_ij = <S_ij S_00>"""
+function getSij!(Sij,Conf::SW.SpinConfig,i,j)
+    S = Conf.Mat
+    si = S[i,j]
+    for k in axes(S,1)
+        for l in axes(S,2)
+            Sij[k,l] = si*S[k,l]
+        end
+    end
+    return Sij
+end
+getSij(Conf::SW.SpinConfig,i,j) = getSij!(similar(Conf.Mat),Conf,i,j)
+function getfft(Conf::SW.SpinConfig)
+    Sij = similar(Conf.Mat)
+    Sq = zeros(ComplexF64,size(Conf.Mat))
+    for i in axes(Conf.Mat,1)
+        for j in axes(Conf.Mat,2)
+            i == j || continue
+            getSij!(Sij,Conf,i,j)
+            FT = fft(Sij)
+            Sq .+= FT
+        end
+    end
+    return real.(Sq)
+end
+
+function plotfft(Conf::SW.SpinConfig)
+    FT = getfft(Conf)
+
+    k = fftfreq(size(Conf.Mat,1))
+    fig = Figure()
+    ax = Axis(fig[1,1],aspect = 1)
+    heatmap!(ax,k,k,real(FT))
+    fig
+end
+plotfft(AFM)
+
+##
+function test(LPx,LPy,PlaquetteList;randBuffSize=1_000_000,maxNumTries = 10_000,deleteRows = 4, randBuffer = rand(1:70,randBuffSize)
+)
+    Lx = 2*LPx+1
+    Ly = 2*LPy+1
+
+    Mat = Matrix{Union{Missing,Float64}}(undef,Lx,Ly)
+    fill!(Mat,missing)
+
+    El = PlaquetteList[begin]
+    P = SW.SpinConfig(Mat,El.S)
+
+    j = 2
+    while j <= Ly-1
+        i = 2
+        while i <= Lx-1 
+        
+            Pij = SW.getPlaquette(P,i,j)
+            
+            tileNums = SW.getPossibleTiles(Pij,PlaquetteList)
+            
+            if isempty(tileNums)
+                jmin = max(j-deleteRows,2)
+                
+                Mat[:,jmin:end] .= missing
+                j = jmin
+                i = 2
+                display(plotSpinConfig(P))
+                continue
+            end
+            T = PlaquetteList[SW.getRanTileNum!(randBuffer,tileNums)]
+            Pij .= T
+            display(plotSpinConfig(P))
+
+            i += 2
+        end
+        j += 2
+
+    end
+    return P
+
+end
+##
+randBuffer = rand(1:70,1_000_000)
+##
+test(8,5,AllAllowedConfigs;randBuffer=copy(randBuffer))
--- src/SpiderWebModel.jl
+++ src/SpiderWebModel.jl
@@ -1,11 +1,11 @@
 module SpiderWebModel
-    using StaticArrays
+    using StaticArrays,Random
 
-    import Base:size,getindex,setindex!,iterate,show
+    import Base:size,getindex,setindex!,iterate,show,copy
 
-    struct SpinConfig{T,MatType <: AbstractMatrix{T}} <:AbstractMatrix{T}
+    struct SpinConfig{T,MatType <: AbstractMatrix{T},T1<:Real} <:AbstractMatrix{T}
         Mat::MatType
-        S::T
+        S::T1
     end
 
     Base.getindex(S::SpinConfig,i,j) = getindex(S.Mat,i,j)
@@ -15,15 +15,16 @@ module SpiderWebModel
     Base.iterate(S::SpinConfig) = iterate(S.Mat)
 
     Base.size(S::SpinConfig) = size(S.Mat)
-
-    Base.show(io::IO, ::MIME"text/plain", S::SpinConfig) = print(io,S.Mat)
-    Base.show(io::IO, S::SpinConfig) = print(io,S.Mat)
-
-    function constraint(S1,S2,S3,S4,S5,S6,S7,S8)
-        S1+S2-S3-S4+S5+S6-S7-S8
+    Base.copy(S::SpinConfig) = SpinConfig(copy(S.Mat),S.S)
+    # Base.show(io::IO, ::MIME"text/plain", S::SpinConfig) = show(io,S.Mat)
+    # Base.show(io::IO, S::SpinConfig) = show(io,S.Mat)
+    
+    constraintSigns() = (1,1,-1,-1,1,1,-1,-1)
+    function constraint(sites)
+        cons = constraintSigns()
+        return sum(sgn*S for (sgn,S) in zip(cons,sites))
     end
 
-    constraint(S) = constraint(S...)
 
     """Order in which the spins are stored in the plaquette corresponding to the numbering convention"""
     plaquetteOrder(S1,S2,S3,S4,S5,S6,S7,S8) = SMatrix{3,3}(S4,S5,S6,S3,missing,S7,S2,S1,S8)
@@ -34,12 +35,8 @@ module SpiderWebModel
         return SpinConfig(Mat,S)
     end
 
-    plaquette(S1,S2,S3,S4,S5,S6,S7,S8) = plaquette((S1,S2,S3,S4,S5,S6,S7,S8))
-    plaquette(x::AbstractVector) = plaquette(x...)
-
     function getSitesFromPlaquette(Mat::AbstractMatrix)
-        S4,S5,S6,S3,_,S7,S2,S1,S8 = Mat
-        return (S1,S2,S3,S4,S5,S6,S7,S8)
+        return (Mat[2,3],Mat[1,3],Mat[1,2],Mat[1,1],Mat[2,1],Mat[3,1],Mat[3,2],Mat[3,3])
     end
 
     function getSitesFromPlaquette(P::SpinConfig) 
@@ -58,7 +55,7 @@ module SpiderWebModel
     """generates all possible combinations 0,1 of the 8 spins in a plaquette """
 
     function getAllGS(S)
-        allCombs = [plaquette(SA[i,j,k,l,m,n,o,p]) for i in -S:S for j in -S:S for k in -S:S for l in -S:S for m in -S:S for n in -S:S for o in -S:S for p in -S:S if constraint(i,j,k,l,m,n,o,p) == 0]
+        allCombs = [plaquette((i,j,k,l,m,n,o,p)) for i in -S:S for j in -S:S for k in -S:S for l in -S:S for m in -S:S for n in -S:S for o in -S:S for p in -S:S if constraint((i,j,k,l,m,n,o,p)) == 0]
         return allCombs
     end
 
@@ -66,43 +63,6 @@ module SpiderWebModel
 
     _isZeroOrMissing(x) = x === missing || x == 0
 
-    function canTileUpRight(T1,T2)
-        T1´ = T1[1:2,2:3]
-        T2´ = T2[2:3,1:2]
-        return all(_isZeroOrMissing,T1´-T2´)
-    end
-
-    function canTileUpLeft(T1,T2)
-        T1´ = T1[1:2,1:2]
-        T2´ = T2[2:3,2:3]
-        return all(_isZeroOrMissing,T1´-T2´)
-    end
-    
-    function canTileDownRight(T1,T2)
-        T1´ = T1[2:3,2:3]
-        T2´ = T2[1:2,1:2]
-        return all(_isZeroOrMissing,T1´-T2´)
-    end
-
-    function canTileDownLeft(T1,T2)
-        T1´ = T1[2:3,1:2]
-        T2´ = T2[1:2,2:3]
-        return all(_isZeroOrMissing,T1´-T2´)
-    end
-    
-    function getTilings(T1,AllTilings)
-        UpRight = [i for (i,T2) in enumerate(AllTilings) if canTileUpRight(T1,T2)]
-        UpLeft = [i for (i,T2) in enumerate(AllTilings) if canTileUpLeft(T1,T2)]
-        DownRight = [i for (i,T2) in enumerate(AllTilings) if canTileDownRight(T1,T2)]
-        DownLeft = [i for (i,T2) in enumerate(AllTilings) if canTileDownLeft(T1,T2)]
-        return (;UpRight,UpLeft,DownRight,DownLeft)
-    end
-
-
-    function setTile!(S::SpinConfig,i::Integer,j::Integer,T1)
-        P = getPlaquette(S,i,j)
-        P .= T1
-    end
 
     function getCorrel(Conf::SpinConfig)
         S_ij = [si*sj for si in Conf.Mat, sj in Conf.Mat]
@@ -111,12 +71,13 @@ module SpiderWebModel
 
     plaquetteOperator() = SpinConfig(
         float(SA[
-            -1 -1 +1;
-            +1  0 +1;
-            +1 -1 -1
+            +1 +1 -1;
+            -1  0 -1;
+            -1 +1 +1
         ]),
         1/2
     )
+
     function PlaquetteOperator!(Conf::SpinConfig, i::Integer,j::Integer)
         P = getPlaquette(Conf,i,j)
         Op = plaquetteOperator()
@@ -135,17 +96,22 @@ module SpiderWebModel
 
     """Assumes that constraint are only defined on every alternating site, starting from the first index"""
     function fulFillsConstraint(Conf::SpinConfig;verbose = false)
-        if any(x->abs(x)>Conf.S,Conf.Mat) 
-            verbose && println("Spin larger than S")
-            return false
+        for x in Conf.Mat
+            ismissing(x) && continue
+            if abs(x) > Conf.S 
+                verbose && println("Spin larger than S")
+                return false
+            end
         end
-        
+
         for i in axes(Conf.Mat,1), j in axes(Conf.Mat,2)
             iseven(i+j) || continue 
             plaquetteIsInBounds(Conf,i,j) || continue
             P = getPlaquette(Conf,i,j)
+            c = constraint(P)
+            ismissing(c) && continue
 
-            if constraint(P) != 0
+            if c ≠ 0
                 verbose && println("Constraint not fulfilled at i,j = $((i,j))" )
                 return false
             end
@@ -185,6 +151,189 @@ module SpiderWebModel
     function flipSpinsAlongRow!(Conf,i)
         Conf[i,:] .*= -1
         return Conf
+    end
+
+    function CanApply(Conf::SpinConfig,Op::SpinConfig,i,j)
+        plaquetteIsInBounds(Conf,i,j) || return false
+        P = getPlaquette(Conf,i,j)
+        
+        P .+= Op
+
+        applicable = fulFillsConstraint(Conf)
+
+        P .-= Op
+
+        return applicable
+    end
+
+    function CanApplyAnywhere(Conf::SpinConfig,Op::SpinConfig)
+        a1 = axes(Conf.Mat,1)
+        a2 = axes(Conf.Mat,2)
+
+        for i in a1, j in a2
+            firstindex(a1)+2 <= i <= lastindex(a1)-2 || continue
+            firstindex(a2)+2 <= j <= lastindex(a2)-2 || continue
+
+            if CanApply(Conf,Op,i,j)
+                return true
+            end
+        end
+        return false
+    end
+    
+    function getApplicablePlaquettes(Conf::SpinConfig,Op::SpinConfig)
+        plaqPos = [(i,j) for i in axes(Conf.Mat,1) for j in axes(Conf.Mat,2) if CanApply(Conf,Op,i,j)]
+        return plaqPos
+    end
+    
+    # function sumNonMissing(sites)
+    #     cons = constraintSigns()
+    #     return sum(sgn*S for (sgn,S) in zip(cons,sites) if !ismissing(S))
+    # end
+
+    # function fillMissingConstraint!(sites)
+    #     numMiss = count(ismissing,Sites)
+    #     g
+    #     return Conf
+    # end
+
+    
+    function canTileUpRight(T1,T2)
+        T1´ = T1[1:2,2:3]
+        T2´ = T2[2:3,1:2]
+        return all(_isZeroOrMissing,T1´-T2´)
+    end
+    
+    function canTileUpLeft(T1,T2)
+        T1´ = T1[1:2,1:2]
+        T2´ = T2[2:3,2:3]
+        return all(_isZeroOrMissing,T1´-T2´)
+    end
+    
+    function canTileDownRight(T1,T2)
+        T1´ = T1[2:3,2:3]
+        T2´ = T2[1:2,1:2]
+        return all(_isZeroOrMissing,T1´-T2´)
+    end
+
+    function canTileDownLeft(T1,T2)
+        T1´ = T1[2:3,1:2]
+        T2´ = T2[1:2,2:3]
+        return all(_isZeroOrMissing,T1´-T2´)
+    end
+    
+    function getTilings(T1,AllTilings)
+        UpRight = [i for (i,T2) in enumerate(AllTilings) if canTileUpRight(T1,T2)]
+        UpLeft = [i for (i,T2) in enumerate(AllTilings) if canTileUpLeft(T1,T2)]
+        DownRight = [i for (i,T2) in enumerate(AllTilings) if canTileDownRight(T1,T2)]
+        DownLeft = [i for (i,T2) in enumerate(AllTilings) if canTileDownLeft(T1,T2)]
+        return (;UpRight,UpLeft,DownRight,DownLeft)
+    end
+
+    function canPlaceTile(P::SpinConfig,T1)
+        return all(_isZeroOrMissing, p -t for (p,t) in zip(P,T1))
+    end
+
+    function setTile!(S::SpinConfig,i::Integer,j::Integer,T1)
+        P = getPlaquette(S,i,j)
+        P .= T1
+    end
+
+    getPossibleTiles(P,PlaquetteList) = [i for (i,t) in enumerate(PlaquetteList) if canPlaceTile(P,t)]
+            
+    function getRanTileNum!(randBuffer,tiles) 
+        isempty(randBuffer) && error("too many steps")
+        maxIndex = length(tiles)
+
+        ranNumber = pop!(randBuffer)
+        idx = ( (ranNumber - 1) % maxIndex) +1
+        if idx == 0 || idx > maxIndex
+            error("idx == $idx for ranNumber = $ranNumber and maxIndex = $maxIndex")
+        end
+
+        return tiles[idx]
+    end
+
+    function constructSpinConfigFromPlaquettes(LPx,LPy,PlaquetteList;randBuffSize=1_000_000,maxNumTries = 10_000,deleteRows = 4, randBuffer = rand(1:70,randBuffSize)
+        )
+        Lx = 2*LPx+1
+        Ly = 2*LPy+1
+    
+        Mat = Matrix{Union{Missing,Float64}}(undef,Lx,Ly)
+        fill!(Mat,missing)
+    
+        El = PlaquetteList[begin]
+        P = SpinConfig(Mat,El.S)
+    
+        j = 2
+        while j <= Ly-1
+            i = 2
+            while i <= Lx-1 
+            
+                Pij = getPlaquette(P,i,j)
+                
+                tileNums = getPossibleTiles(Pij,PlaquetteList)
+                
+                if isempty(tileNums)
+                    jmin = max(j-deleteRows,2)
+                    
+                    Mat[:,jmin:end] .= missing
+                    j = jmin
+                    i = 2
+                    continue
+                end
+                T = PlaquetteList[getRanTileNum!(randBuffer,tileNums)]
+                Pij .= T
+    
+                i += 2
+            end
+            j += 2
+    
+        end
+        @assert fulFillsConstraint(P,verbose=true) "initial configuration does not fulfill constraint"
+
+        OldState = copy(P)
+        for i in 1:randBuffSize
+            i % 100 == 0 && println("i = $i")
+            randPlaqlist = shuffle(PlaquetteList)
+            fillMissing!(P,randPlaqlist)
+            if !any(ismissing,P) 
+                @assert fulFillsConstraint(P,verbose = true) "constraint not fulfilled"
+                return P
+            end
+            P.Mat .= OldState.Mat
+        end
+
+        fulFillsConstraint(P,verbose = true) || warn("constraint not fulfilled")
+        return P
+    end
+
+    function fillMissing!(P::SpinConfig,PlaquetteList)
+    
+        OldPij = Matrix{Union{Missing,Float64}}(undef,3,3)
+        fill!(OldPij,missing)
+        Lx,Ly = size(P.Mat)
+        for i in 3:2:Lx-2, j in 3:2:Ly-2
+
+            Pij = getPlaquette(P,i,j)
+            OldPij .= Pij
+            @assert fulFillsConstraint(P,verbose = true) "constraint not fulfilled in fillMissing"
+            
+            for T in PlaquetteList
+                
+                if canPlaceTile(Pij,T)
+                    Pij.Mat .= T
+                    if fulFillsConstraint(P)
+                        break
+                    else 
+                        Pij.Mat .= OldPij
+                    end
+                end
+            end
+            @assert fulFillsConstraint(P,verbose = true) "constraint not fulfilled in fillMissing2"
+        end
+        
+        return P
     end
 
 end # module SpiderWebModel
