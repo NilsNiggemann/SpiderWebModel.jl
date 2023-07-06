021d28f2e78b104c89920db3388b25c9ddc6ebb9
Nils <nils.niggemann@fu-berlin.de>
2023-07-06 17:13:22 +0200

    couplings

---
+++ Application/Couplings.jl
@@ -0,0 +1,97 @@
+using SpinFRGLattices
+using SpinFRGLattices.Octochlore:spin
+using SpinFRGLattices.StructArrays
+
+##
+
+function getCouplingsToS1(RefSite = Rvec(0,0,1))
+    L=4
+    S1Terms = spin{Int,Rvec_2D}[]
+    for n1 in -L+RefSite.n1:L+RefSite.n1,n2 in -L+RefSite.n2:L+RefSite.n2
+        isodd(n1+n2) && continue
+        sites = [Rvec(n1+1,n2,1),Rvec(n1+1,n2+1,1),Rvec(n1,n2+1,1),Rvec(n1-1,n2+1,1),Rvec(n1-1,n2,1),Rvec(n1-1,n2-1,1),Rvec(n1,n2-1,1),Rvec(n1+1,n2-1,1)]
+        sgns = [1,1,-1,-1,1,1,-1,-1]
+        plaq = [spin(r,s) for (r,s) in zip(sgns,sites)]
+        for s1 in plaq
+            for s2 in plaq
+                if s1.site == RefSite
+                    push!(S1Terms,spin(s1.fac*s2.fac,s2.site))
+                elseif s2.site == RefSite
+                    push!(S1Terms,spin(s1.fac*s2.fac,s1.site))
+                end
+            end
+        end
+    end
+    return StructArray(S1Terms)
+end
+
+##
+using DataFrames
+
+##
+p0 = Point2f(1,0)
+a = SpinFRGLattices.Octochlore.reduceCouplings(getCouplingsToS1(Rvec(p0...,1)))
+filter!(x->x.fac != 0,a)
+filter!(x->x.site != Rvec(p0...,1),a)
+aDF = DataFrame(;n1 = StructArray(a.site).n1, n2 = StructArray(a.site).n2,fac = a.fac)
+##
+let
+    p0 = Point2f(0,0)
+    a = SpinFRGLattices.Octochlore.reduceCouplings(getCouplingsToS1(Rvec(p0...,1)))
+    filter!(x->x.fac != 0,a)
+    filter!(x->x.site != Rvec(p0...,1),a)
+
+    aDF = DataFrame(;n1 = StructArray(a.site).n1, n2 = StructArray(a.site).n2,fac = a.fac)
+
+    p = [Point2f(n1,n2) for (n1,n2) in zip(aDF.n1,aDF.n2)]
+    fig = Figure()
+    ax = Axis(fig[1,1],aspect = 1,xgridcolor = :black,ygridcolor = :black,backgroundcolor = :white)
+    col = Dict(0=>:black,2 => :red, -2 => :blue,4 => :darkred, -4 => :darkblue)
+    ls(i) = i >0 ? :solid : :dash
+    for (i,p) in enumerate(p)
+        fac = aDF.fac[i]
+        lines!([p0,p],linewidth=5*abs(fac),color=col[fac],linestyle = ls(fac) ,label = string(fac))
+    end
+    scatter!([Point2f(n1,n2)+p0 for n1 in -2:2 for n2 in -2:2],color = :black,markersize = 10)
+    scatter!(p,color = :black,markersize = 50,marker = '×')
+
+    axislegend(ax,unique=true)
+
+    save("couplings_$(Tuple(Int.(p0))).png",fig)
+    fig
+end
+
+##
+
+let
+    L = 1
+    AllPoints = [Point2f(n1,n2) for n1 in -L:L for n2 in -L:L]
+    
+    fig = Figure()
+    ax = Axis(fig[1,1],aspect = 1,xgridcolor = :black,ygridcolor = :black,backgroundcolor = :white)
+
+    PlotPoints = Set(copy(AllPoints))
+    for p0 in AllPoints
+        a = SpinFRGLattices.Octochlore.reduceCouplings(getCouplingsToS1(Rvec(p0...,1)))
+        filter!(x->x.fac != 0,a)
+        aDF = DataFrame(;n1 = StructArray(a.site).n1, n2 = StructArray(a.site).n2,fac = a.fac)
+
+        p = [Point2f(n1,n2) for (n1,n2) in zip(aDF.n1,aDF.n2)]
+
+        col = Dict(0=>:black,2 => :red, -2 => :blue,4 => :darkred, -4 => :darkblue)
+
+        for (i,p) in enumerate(p)
+            lines!([p0,p],linewidth=1*abs(aDF.fac[i]),color=col[aDF.fac[i]],label = string(aDF.fac[i]))
+            push!(PlotPoints,p)
+        end
+    end
+    scatter!(collect(PlotPoints),color = :black,markersize = 10)
+
+    axislegend(ax,unique=true)
+
+    save("couplings_All.png",fig)
+    fig
+end
+# for (s,f) in zip(a.site,a.fac)
+#     println(s.n1," ",s.n2," :",f)
+# end
