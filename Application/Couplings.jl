a2ff6bf4ef171afc3999ac73f642fef2b1020346
Nils <nils.niggemann@fu-berlin.de>
2023-07-08 13:43:31 +0200

    large N

--- Application/Couplings.jl
+++ Application/Couplings.jl
@@ -1,7 +1,7 @@
 using SpinFRGLattices
 using SpinFRGLattices.Octochlore:spin
 using SpinFRGLattices.StructArrays
-
+using CairoMakie
 ##
 
 function getCouplingsToS1(RefSite = Rvec(0,0,1))
@@ -57,7 +57,7 @@ let
 
     axislegend(ax,unique=true)
 
-    save("couplings_$(Tuple(Int.(p0))).png",fig)
+    save("couplings_$(Tuple(Int.(p0))).pdf",fig)
     fig
 end
 
@@ -92,6 +92,67 @@ let
     save("couplings_All.png",fig)
     fig
 end
-# for (s,f) in zip(a.site,a.fac)
-#     println(s.n1," ",s.n2," :",f)
-# end
+##
+using LargeN
+using LargeN.StaticArrays
+function SpiderWebBasis()
+    a1 = SA[1,1]
+    a2 = SA[-1,1]
+    b = [SA[0,0],SA[1,0]]
+    return Basis_Struct_2D(;a1,a2,b,NNdist = 1.,NUnique =2)
+end
+
+let 
+    B = SpiderWebBasis()
+    L = 2
+    
+    points = [Point2f(getCartesian(Rvec(n1,n2,b),B)...) for n1 in -L:L for n2 in -L:L for b in 1:2]
+    fig = Figure()
+    ax = Axis(fig[1,1],aspect = 1)
+    scatter!(points,color = :black,markersize = 10)
+    fig
+end
+##
+
+
+function getJMatrix(q)
+    qx,qy = q
+    
+    J11 = 2cos(2(qx+qy))+2cos(2(qx-qy))-4cos(2*qx)-4cos(2*qy)
+    J12 = +2cos(2qx+qy) + 2cos(qx -2qy) - 2cos(2qx-qy) - 2cos(qx+2qy)
+    J21 = J12
+    J22 = -4cos(qx+qy)-4cos(qx-qy) +2cos(2qx) + 2cos(2qy)
+    J= LargeN.Hermitian(SA[
+        J11 J12;
+        J21 J22
+    ])
+    return J
+end
+
+LargeN.getNCell(::typeof(getJMatrix),) = 2
+
+##
+let 
+    res = 30
+    q = LinRange(-pi,pi,res)
+    eig = LargeN.ComputeEig2D(getJMatrix,res,ext = pi,2)
+    fig = Figure()
+    ax = Axis3(fig[1,1],aspect = (1,1,1/4))
+    surface!(ax,q,q,eig.values[1,:,:],colorrange = extrema(eig.values),transparency = true)
+    surface!(ax,q,q,eig.values[2,:,:],colorrange = extrema(eig.values),transparency = true,colormap = (:viridis,0.6))
+    save("eig.png",fig)
+    fig
+end
+##
+
+let 
+    # chi = getChiFunction(0.0001,getJMatrix,2,2;BZextent = float(2pi),nk=80)
+    chi = getZeroTChi(getJMatrix)
+    q = LinRange(-pi,pi,700)
+    fig = Figure()
+    ax = Axis(fig[1,1],aspect = 1)
+    chiq = [chi(q1,q2) for q1 in q, q2 in q]
+    heatmap!(ax,q,q,chiq)
+    save("chi.png",fig)
+    fig
+end
