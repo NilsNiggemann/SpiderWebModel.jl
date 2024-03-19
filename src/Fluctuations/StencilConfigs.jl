2c2ae1b62ead209ea8cfb746b6c7a44800eef4e6
Nils Niggemann <nils.niggemann@fu-berlin.de>
2024-03-19 11:09:17 +0100

    improve performance with Int8

--- src/Fluctuations/StencilConfigs.jl
+++ src/Fluctuations/StencilConfigs.jl
@@ -1,5 +1,5 @@
-function stencilConfig(A::AbstractArray,S::Real;kwargs...)
-    St = Stencils.StencilArray(A,Stencils.Moore();boundary = Stencils.Remove(NaN),kwargs...)
+function stencilConfig(A::AbstractArray,S::T;kwargs...) where T
+    St = Stencils.StencilArray(A,Stencils.Moore();boundary = Stencils.Remove(T(typemax(T))),kwargs...)
     return SpinConfig(St,S)
 end
 
@@ -22,54 +22,36 @@ end
 
 @inline Base.@propagate_inbounds getPlaquetteSites(S::SpinConfig,i::Int,j::Int) = getPlaquetteSites(parent(S),i,j)
 
-
-@inline function P_plus_applicable(Pij::Union{SVector{8},Stencils.Moore},Spin::Real)
+"""returns a tuple of two booleans, corresponding to the applicability of P and P⁺. M=2S is an integer"""  
+@inline function P_applicable(Pij::Union{SVector{8},Stencils.Moore},M::Integer)
     S1, S2, S3, S4, S5, S6, S7, S8 = Pij
-    max(S1,-S2,-S3,S4,S5,-S6,-S7,S8) <= Spin-1
+    v = (S1,-S2,-S3,S4,S5,-S6,-S7,S8)
+
+    p = all(<(M),v)
+    pdagger = all(>(-M),v)
+    return p ,pdagger
 end
 
-@inline function P_minus_applicable(Pij::Union{SVector{8},Stencils.Moore},Spin::Real)
-    S1, S2, S3, S4, S5, S6, S7, S8 = Pij
-    max(-S1,S2,S3,-S4,-S5,S6,S7,-S8) <= Spin-1
-end
-
-@inline function P_plus_applicable(S::SpinConfig, i::Int, j::Int)
+@inline function P_applicable(S::SpinConfig, i::Int, j::Int)
     Pij = getPlaquetteSites(S, i, j)
-    P_plus_applicable(Pij, S.S)
+    P_applicable(Pij, Int8(1))
 end
-@inline function P_minus_applicable(S::SpinConfig, i::Int, j::Int)
-    Pij = getPlaquetteSites(S, i, j)
-    P_minus_applicable(Pij, S.S)
-end
-@inline P_plus_applicable(S::SpinConfig, I::Union{<:NTuple{2},<:CartesianIndex{2}}) = P_plus_applicable(S, I[1], I[2])
-@inline P_minus_applicable(S::SpinConfig, I::Union{<:NTuple{2},<:CartesianIndex{2}}) = P_plus_applicable(S, I[1], I[2])
 
-function getApplicablePlaquettes!(plaqsPos,Conf::SpinConfig{T,<:Stencils.StencilArray},SignFlip::Bool) where T
-    empty!(plaqsPos)
-    if SignFlip
-        for I in plaquetteIterator(Conf)
-            if P_plus_applicable(Conf, I)
-                push!(plaqsPos, Tuple(I))
-            end
+@inline P_applicable(S::SpinConfig, I::Union{<:NTuple{2},<:CartesianIndex{2}}) = P_applicable(S, I[1], I[2])
+
+function getApplicablePlaquettes!(plaqsPlus,plaqsMinus,Conf::SpinConfig{T,<:Stencils.StencilArray}) where T
+    empty!(plaqsPlus)
+    empty!(plaqsMinus)
+    for I in plaquetteIterator(Conf)
+        applPlus,applMinus = P_applicable(Conf, I)
+        if applPlus
+            push!(plaqsPlus, Tuple(I))
         end
-    else
-        for I in plaquetteIterator(Conf)
-            if P_minus_applicable(Conf,I)
-                push!(plaqsPos, Tuple(I))
-            end
+        if applMinus
+            push!(plaqsMinus, Tuple(I))
         end
     end
-    return plaqsPos
+    return plaqsPlus,plaqsMinus
 end
 
-function getApplicablePlaquettes2!(plaqsPos,Conf::SpinConfig{T,<:Stencils.StencilArray},SignFlip::Bool) where T
-    if SignFlip
-        Stencils.mapstencil!(x->P_plus_applicable(x,Conf.S),plaqsPos,parent(Conf))
-    else
-        Stencils.mapstencil!(x->P_minus_applicable(x,Conf.S),plaqsPos,parent(Conf))
-    end
-    return plaqsPos
-end
-
-getApplicablePlaquettes(Conf::SpinConfig{T,<:Stencils.StencilArray},SignFlip::Bool) where T = getApplicablePlaquettes!(Vector{Tuple{Int,Int}}(),Conf,SignFlip)
-getApplicablePlaquettes2(Conf::SpinConfig{T,<:Stencils.StencilArray},SignFlip::Bool) where T = getApplicablePlaquettes2!(deepcopy(parent(Conf)),Conf,SignFlip)
+getApplicablePlaquettes(Conf::SpinConfig{T,<:Stencils.StencilArray}) where T = getApplicablePlaquettes!(Vector{Tuple{Int,Int}}(),Vector{Tuple{Int,Int}}(),Conf)
