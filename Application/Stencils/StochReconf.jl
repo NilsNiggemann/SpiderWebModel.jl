b130f2546fb256dbdfb07fef5b609d98f01bd4de
Nils Niggemann <nils.niggemann@fu-berlin.de>
2024-05-25 15:25:14 +0200

    further performance improvement

--- Application/Stencils/StochReconf.jl
+++ Application/Stencils/StochReconf.jl
@@ -7,17 +7,21 @@ using MakieHelpers
 using SpiderWebModel
 
 ##
-S = SW.stencilConfig(zeros(12,12),1;
+S = SW.stencilConfig(zeros(15,15),1;
 # boundary = SW.Stencils.Wrap(),padding = SW.Stencils.Conditional()
 )
 ψG = SW.constructVariationalFunction(S,0.15)
 # SW.get_beta_ij(ψG) .= 0.01
 nThermal = 100
-
 ##
-SW.repeatStochReconf(S,200,ψG,5,5e-2;Nwalkers = 12,nbra = 10,error_threshold=1e-1,equilibration_steps=nThermal)
+res = SW.startManyWalkerGFMC(S,6,100,3,ψG,1;equilibration_steps=nThermal)
 ##
-stochReconfRes = SW.repeatStochReconf(S,200,ψG,10,5e-2;Nwalkers = 12,nbra = 10,error_threshold=1e-1,equilibration_steps=nThermal)
+a = SW.reconf_obs(S,eachslice(res.SaveConfigs,dims=(3,4)),ψG,1)
+##
+@profview SW.repeatStochReconf(S,100,ψG,5,5e-2;Nwalkers = 12,nbra = 10,error_threshold=1e-1,equilibration_steps=nThermal)
+##
+SW.Random.seed!(1234)
+stochReconfRes = SW.repeatStochReconf(S,200,ψG,15,5e-2;Nwalkers = 24,nbra = 30,error_threshold=1e-1,equilibration_steps=nThermal)
 ##
 let 
     fig = Figure()
@@ -32,12 +36,15 @@ let
     fig
 end
 ##
-# ψGnew = SW.VariationalGuidingFunction(stochReconfRes.params)
+ψGnew = SW.VariationalGuidingFunction(stochReconfRes.params)
 ψGold = SW.PlaquetteNumberGuidingFunction(0.15)
 ##
 nBra = 6
 @time results = fetch.([Threads.@spawn SW.startManyWalkerGFMC(S,5,50_000÷nBra,nBra,ψGnew,1;equilibration_steps=nThermal) for _ in 1:24])
+##
+@time resultsOld = fetch.([Threads.@spawn SW.startManyWalkerGFMC(S,5,50_000÷nBra,nBra,ψGold,1;equilibration_steps=nThermal) for _ in 1:24])
 
 ##
-# plotEnergies(results,nBra,-20.35;Emin=-20.5,Emax=-19.8)
-plotEnergies(results,nBra,-49.7;Emin=-50.5,Emax=-46)
+# plotEnergies(results,nBra,-20.35;Emin=-20.5,Emax=-19.8) # L=10
+plotEnergies(resultsOld,nBra,NaN)
+# plotEnergies(results,nBra,-49.7;Emin=-50.5,Emax=-46)
--- src/Fluctuations/StochasticReconfiguration.jl
+++ src/Fluctuations/StochasticReconfiguration.jl
@@ -10,20 +10,11 @@ function reconf_obs(InitialState::ConfType,configs,ψG,Λ) where {ConfType}
     AffectedPlaquetteList = precomputeAffectedPlaquettes(InitialState)
     
     NThreads = Threads.nthreads()
-    # NThreads = 1
 
     Nparams = length(ψG.params)
+    Oik = zeros(length(configs),Nparams)
+    E_i = zeros(length(configs))
     
-    O_sum_buffer = [zeros(Nparams) for _ in 1:NThreads]
-    O²_sum_buffer = [zeros(Nparams,Nparams) for _ in 1:NThreads]
-    EL_sum_buffer = zeros(NThreads)
-    OEL_sum_buffer = [zeros(Nparams) for _ in 1:NThreads]
-
-    Walkers = Vector{SpiderWebWalker{ConfType}}(undef,NThreads)
-    Threads.@threads for α in eachindex(Walkers)
-        Walkers[α] = spiderWebWalker(InitialState,plaqs)
-    end
-
     PseudoParametersMatrices = [
         singleElementmatrix(size(ψG.params),i,1.) for i in 1:Nparams
     ]
@@ -32,12 +23,7 @@ function reconf_obs(InitialState::ConfType,configs,ψG,Λ) where {ConfType}
     
     Threads.@threads for (ichunk,chunkinds) in enumerate(WorkChunks)
 
-        Walker = Walkers[ichunk]
-        O_sum = O_sum_buffer[ichunk]
-        O²_sum = O²_sum_buffer[ichunk]
-        OEL_sum = OEL_sum_buffer[ichunk]
-
-        O_x = zeros(length(chunkinds),Nparams)
+        Walker = spiderWebWalker(InitialState,plaqs)
 
         @inbounds for (i,iconf) in enumerate(chunkinds)
             get_config(Walker) .= configs[iconf]
@@ -47,43 +33,23 @@ function reconf_obs(InitialState::ConfType,configs,ψG,Λ) where {ConfType}
             for k in 1:Nparams
                 PseudoGuidingWF = VariationalGuidingFunction(PseudoParametersMatrices[k])
                 O_xk = guidingfunc_exponent(PseudoGuidingWF,Walker.n_x)
-                O_x[i,k] = O_xk
-                OEL_sum[k] += O_xk*elocal
-                O_sum[k] += O_xk
+                Oik[iconf,k] = O_xk
             end
-            EL_sum_buffer[ichunk] += elocal
+            E_i[iconf] = elocal
         end
 
-        @inbounds for k in 1:Nparams
-            for j in 1:Nparams
-                O²_sumval = 0.
-                for (i,iconf) in enumerate(chunkinds)
-                    O²_sumval += O_x[i,k] * O_x[i,j]
-                end
-                O²_sum[j,k] = O²_sumval
-            end
-        end
-        # O²_sum .+= O²_sum'
-        # O²cov_sum .= naiveCov(O_x)
-        # @inbounds for k in 1:Nparams
-        #     for j in k+1:Nparams
-        #         O²cov_sum[k,j] = O²cov_sum[j,k]
-        #     end
-        # end
     end
     N = length(configs)
-    O_avg = sum(O_sum_buffer)./N
-    O²_avg = sum(O²_sum_buffer)./N
-    EL_avg = sum(EL_sum_buffer)./N
-    OEL_avg = sum(OEL_sum_buffer)./N
-    return (;O_avg,O²_avg,EL_avg,OEL_avg)
+    EL_avg = mean(E_i)
+    
+    S = cov(Oik,dims=1) + 1e-5I
+    F = cov(Oik,E_i)
+
+    return (;EL_avg,S,F)
 end
 
 function stochastic_reconfiguration(InitialState,configs,ψG,Λ)
-    (;O_avg,O²_avg,EL_avg,OEL_avg) = reconf_obs(InitialState,configs,ψG,Λ)
-    F = OEL_avg .- O_avg .* EL_avg
-    S = O²_avg - O_avg * O_avg' + 1e-5I
-    # S = O²_avg + 1e-5I
+    (;EL_avg,F,S) = reconf_obs(InitialState,configs,ψG,Λ)
     return δα = S \ F
 end
 
