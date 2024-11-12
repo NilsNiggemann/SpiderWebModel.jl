3188af234a4984f56d6d5944f58713ab7f50171f
Nils Niggemann <nils.niggemann@fu-berlin.de>
2024-11-12 13:37:51 +0100

    RK function

--- src/Variational/VariationalFunctions/RKFunction.jl
+++ src/Variational/VariationalFunctions/RKFunction.jl
@@ -1,20 +1,21 @@
 struct RKFunction <: AbstractGuidingFunction end
+(psi::RKFunction)(W::SpiderWebWalker) = 1.
 
 guidingfunc_name(F::RKFunction) = "RKFunction"
-variational_parameters(::RKFunction) = Dict{Symbol,Float64}()
-guidingfuncRatio_exponent(ψG::RKFunction,Walker::SpiderWebWalker,affectedPlaquettes) = 0.
-function updateWeightList!(Walker::SpiderWebWalker,AffectedPlaquetteList,ψG::RKFunction,Λ=0)
-    (;Config,weights,moves,n_x,n_x´) = Walker
-    isempty(weights) && isempty(moves) || return weights #return if weights are already computed
+get_params(ψG::RKFunction) = nothing
+guidingfuncRatio(ψG::RKFunction,Walker::SpiderWebWalker,move,Buffer::Number) = 1.
+# function updateWeightList!(Walker::SpiderWebWalker,AffectedPlaquetteList,ψG::RKFunction,Λ=0)
+#     (;Config,weights,moves,n_x,n_x´) = Walker
+#     isempty(weights) && isempty(moves) || return weights #return if weights are already computed
     
-    getMoves!(Walker)
+#     getMoves!(Walker)
     
-    resize!(weights,length(moves))
-    weights .= 1.
+#     resize!(weights,length(moves))
+#     weights .= 1.
 
-    if Λ != 0
-        push!(moves, DIAGONAL_MOVE_ID)
-        push!(weights,Λ)
-    end
-    return weights
-end
+#     if Λ != 0
+#         push!(moves, DIAGONAL_MOVE_ID)
+#         push!(weights,Λ)
+#     end
+#     return weights
+# end
