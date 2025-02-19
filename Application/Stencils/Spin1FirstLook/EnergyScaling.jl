1b3ea8711b5bdd3909daa0983cc534bf99083d67
Nils Niggemann <nils.niggemann@fu-berlin.de>
2025-02-19 20:06:05 +0100

    individual files for energy scaling

--- Application/Stencils/Spin1FirstLook/EnergyScaling.jl
+++ Application/Stencils/Spin1FirstLook/EnergyScaling.jl
@@ -46,7 +46,8 @@ function optimizeWF!(ψG,S,CTSR,nThermal;dt = 1e-4,NSteps = 100,NConfs = 10,show
     while E0[end] > E0[begin]
         @warn "Optimization failed, reducing dt and increasing NSteps"
         idx += 1
-        idx > 5 && error("Optimization failed")
+        # idx > 5 && error("Optimization failed")
+        idx > 4 && break
         dt *= 0.75
         NSteps += 2NSteps_base
         SW.rand!(ψG,1e-6)
@@ -59,45 +60,36 @@ function optimizeWF!(ψG,S,CTSR,nThermal;dt = 1e-4,NSteps = 100,NConfs = 10,show
 end
 ##
 
-function getEnergies(S,mus,Symmetry;Nwalkers = 20 * 3,NSteps = 2000,nThermal = 1000,NRuns=20,dt = 3e-3,NConfs = 20,nopt1 = 100,nopt2 = 30)
+function getEnergy(S,mu,Symmetry;Nwalkers = 20 * 3,NSteps = 2000,nThermal = 1000,NRuns=20,dt = 3e-3,NConfs = 20,nopt=1000,pre_init_steps=800)
     ψG = initializeGWF(S,Symmetry)
-    ens = zeros(length(mus),NRuns)
-    for (i,mu) in enumerate(mus)
-        println("mu = $mu")
-        
-        tau = 0.1 + 0.1*mu
-        CTSR = SW.ContinuousTimeMethod(30*tau,w_avg_estimate = 0.2*length(S),Hxx = SW.Hxx_RK(mu))
-        
-        if (i-1) %10 == 0
-            nopt = nopt1
-            SW.rand!(ψG,1e-6)
-        else
-            nopt = nopt2
-        end
-        @time E0_est = optimizeWF!(ψG,S,CTSR,nThermal÷5;NSteps = nopt,NConfs,dt)
-        flush(stdout)
-        flush(stderr)
+    SW.rand!(ψG,1e-6)
+    tau = 0.1 + 0.1*mu
 
-        
-        CT = SW.ContinuousTimeMethod(tau,w_avg_estimate = E0_est,Hxx = CTSR.Hxx)
-        initializer = SW.VariableTimePropagation(LinRange(10,tau,800),CT)
+    CTSR = SW.ContinuousTimeMethod(30*tau,w_avg_estimate = 0.2*length(S),Hxx = SW.Hxx_RK(mu))
 
-        @time results_en = fetch.([Threads.@spawn SW.measure_Sq_GFMC(S,CT,Nwalkers,NSteps,50,ψG.psi,estimate_w_avg=true,equilibration_steps=nThermal,initializer=initializer).Energy for _ in 1:NRuns])
-        flush(stdout)
-        flush(stderr)
-        # @time results_en = SW.measure_Sq_GFMC(S,CT,Nwalkers,NSteps,50,ψG.psi,equilibration_steps=NSteps÷5, estimate_w_avg=true)
-        # return results_en
-        # display(plotEnergies(results,CT))
-        en = stack(results_en)[end,:]
-        ens[i,:] .= en
+    @time E0_est = optimizeWF!(ψG,S,CTSR,nThermal÷5;NSteps = nopt,NConfs,dt)
 
-    end
-    return ens
+    flush(stdout)
+    flush(stderr)
+
+    CT = SW.ContinuousTimeMethod(tau,w_avg_estimate = E0_est,Hxx = CTSR.Hxx)
+    initializer = SW.VariableTimePropagation(LinRange(10,tau,pre_init_steps),CT)
+
+    @time results = fetch.([Threads.@spawn SW.measure_Sq_GFMC(S,CT,Nwalkers,NSteps,50,ψG.psi,estimate_w_avg=true,equilibration_steps=nThermal,initializer=initializer) for _ in 1:NRuns])
+
+    results_en = [r.Energy[end] for r in results]
+
+    results_sf = stack([r.StructureFactor[:,:,end] for r in results])
+
+    flush(stdout)
+    flush(stderr)
+
+    return results_en,results_sf
 end
 ##
 
 sector_nums = [1,2,3,4,5,6,8,10]
-muRange = reverse(LinRange(-0.1,0.99,30))
+muRange = LinRange(-0.1,0.99,30)
 
 L = 20
 Symms = [
@@ -110,11 +102,7 @@ Symms = [
     SW.TranslationalSymmetry([2,2],[2,-2]), #8
     SW.TranslationalSymmetry([4,0],[0,4]), #10
 ]
-outfile_EnergyScaling = "../../Data/energy_mu_S1_3.h5"
-
-##
-
-# outfile_EnergyScaling = "../../Data/energy_mu_S1.h5"
+output_dir = "../../Data/EnergyScaling_S1_L$L"
 
 function get_keys(file)
     h5open(file,"r") do f
@@ -122,78 +110,62 @@ function get_keys(file)
     end
 end
 
-
-h5open(outfile_EnergyScaling,"cw") do f
-    
-    pres_keys = keys(f)
-    if "muRange" in pres_keys
-        if f["muRange"][:] != Array(muRange)
-            rm(outfile_EnergyScaling)
-            f["muRange"] = Array(muRange)
-        end
-    else    
-        f["muRange"] = Array(muRange)
-    end
-end
-
-mkpath(dirname(outfile_EnergyScaling))
+mkpath(output_dir)
 
 println("start main loop")
 flush(stdout)
 flush(stderr)
 
-# for (sector,Symm) in zip(sector_nums,Symms)
-for idx in eachindex(sector_nums,Symms)[1:1]
+for idx in eachindex(sector_nums,Symms)
     sector = sector_nums[idx]
-    print("Sector $sector")
-
-    present_keys = get_keys(outfile_EnergyScaling)
-    @info "" present_keys
-    if string(sector) in present_keys
-        println(" skipped")
-        flush(stdout)
-        continue
-    end
-    println(" started")
+    println("Sector $sector")
     flush(stdout)
-    
+
+    sector_dir = joinpath(output_dir, "sector_$sector")
+    mkpath(sector_dir)
+
     Symm = Symms[idx]
     S = SW.get4x4PeriodicSpinConf(L,sector)
-    h5write(outfile_EnergyScaling,"$sector/conf", Array(S))
 
-    SW.Random.seed!(1232)
-    
-    Nwalkers = 28*20
-    nThermal = 1000
+    for mu in muRange
+        mu_file = joinpath(sector_dir, "mu_$(round(mu, digits=3)).h5")
+        if isfile(mu_file)
+            println("mu = $mu skipped")
+            continue
+        end
+        try
+            h5write(mu_file, "sector", sector)
+            h5write(mu_file, "mu", mu)
+            
+        catch e
+            @warn e
+            continue
+        end
+        println("mu = $mu started")
+        flush(stdout)
 
-    # if sector == 1
-    #     Nwalkers *=40
-    #     nThermal = 4000
-    # end
-    e_i = getEnergies(S,muRange,Symm;Nwalkers,NSteps = 3000,nThermal,NRuns=12,dt = 1e-3,NConfs = 50,nopt1 = 1500,nopt2 = 500)
-    
-    h5write(outfile_EnergyScaling,"$sector/energy",e_i)
+        SW.Random.seed!(1232)
+        
+        Nwalkers = 28*5
+        nThermal = 500
+        NSteps = 3000
+        NRuns = 12
+        nopt=1500
+        NConfs=50
+        pre_init_steps = 500
+
+        if sector == 1
+            Nwalkers *=20
+            nThermal = 4000
+            pre_init_steps = 1200
+        end
+        
+        results_en, results_sf = getEnergy(S, mu, Symm; Nwalkers, NSteps, nThermal, NRuns, dt = 8e-3, NConfs, nopt,pre_init_steps)
+
+        h5write(mu_file, "energies", results_en)
+        h5write(mu_file, "structure_factors", results_sf)
+        println("mu = $mu done")
+    end
     println("Sector $sector done")
 end
-exit()
-##
 
-
-function recoverCorrupt(fname,newfile,keys)
-    try
-        muRange = h5read(fname,"muRange")
-        h5write(newfile,"muRange",muRange)
-        
-    catch
-    end
-    for key in keys, key2 in ("conf","energy")
-        try
-            data = h5read(fname,"$key/$key2")
-            h5write(newfile,"$key/$key2",data)
-        catch
-
-        end
-    end
-
-end
-recoverCorrupt(outfile_EnergyScaling,"../../Data/energy_mu_S1_recovered.h5",["1","2","3","4","5","6","8","10"])
--- Application/Stencils/Spin1FirstLook/FirstLookPlot.jl
+++ Application/Stencils/Spin1FirstLook/FirstLookPlot.jl
@@ -13,75 +13,112 @@ SqsGFMC = [h5read(file,"StructureFactor") for file in outfiles_Sq]
 # errlines(mean(EnGFMC),std(EnGFMC))
 ##
 
-function plot_overview(outfile_en,Sqs)
-    fig = Figure(size = (700, 500),fontsize = 22)
-    
-    EnergyFig = fig[2,1:3] = GridLayout()
-    SqFig = fig[2,4] = GridLayout()
-    toprow = fig[1,1:4] = GridLayout()
-    # Top row for configurations
 
-    h5open(outfile_en, "r") do f
-        
-        sectors = sort(filter(!=("muRange"),keys(f)),by=x->parse(Int,x))
-        # e_start = [haskeymean(f["$sector/energy"][1,:]) for sector in sectors]
+using DataFrames
 
-        # perm = sortperm(e_start)
-        # sectors = sectors[perm]
+function collect_results(output_dir)
+    sectorsDirs = readdir(output_dir)
+    Sectors = Int[]
+    mus = Float64[]
+    Energy = Vector{Float64}[]
+    StructureFactor = Array{Float64, 3}[]
 
-        colors = Makie.ColorSchemes.distinguishable_colors(length(sectors), lchoices = LinRange(0,80,100))
+    for sector in sectorsDirs
+        sector_dir = joinpath(output_dir, sector)
+        mu_files = readdir(sector_dir)
 
-        for (i, sector) in enumerate(sectors)
-            conf = read(f["$sector/conf"])
-            if sector == "6"
-                conf .= 4SW.getStairCase(size(conf,1)) #plot the staircase state, which is in the same sector
+        for mu_file in mu_files
+            file_path = joinpath(sector_dir, mu_file)
+
+            h5open(file_path, "r") do f
+                if all(k -> k in keys(f), ("mu", "energies", "structure_factors"))
+                    mu = read(f["mu"])
+                    energies = read(f["energies"])
+                    structure_factors = read(f["structure_factors"])
+                    sector = read(f["sector"])
+                    push!(mus, mu)
+                    push!(Sectors, sector)
+                    push!(Energy, energies)
+                    push!(StructureFactor, structure_factors)
+                end
             end
-            S = SW.stencilConfig(conf[1:8,1:8],1)
-            toprow[1,i] = ax_conf = Axis(fig, title=L"%$i";SW.getConfigAxis(S)...,
-            yticklabelsvisible=false,
-            xticklabelsvisible=false,
-            spinecolors(colors[i])...,
-            spinewidth = 4
-            )
-            SW.plotSpinConfig!(ax_conf, S)
-            colgap!(toprow, 0)
         end
-
-        # Bottom row for energies
-        EnergyFig[1,1:8] = ax = with_theme(theme_SimpleTicks()) do
-            Axis(fig, xlabel=L"\mu", 
-            ylabel=L"E_0/N_{\text{sites}}"
-            # ylabel=L"E_0/(N_{\text{sites}}(1-\mu))"
-            )
-        end
-        L = 20
-        muRange = f["muRange"][:]
-        for (i, sector) in enumerate(sectors)
-            "energy" in keys(f[sector]) || continue
-            Nsites = length(f[sector*"/conf"])
-            energies = read(f["$sector/energy"])
-            mean_energies = dropmean(energies, dims=2) ./Nsites# ./ (1 .-muRange)
-            std_energies = dropstd(energies, dims=2) ./Nsites# ./ (1 .-muRange)
-
-            errlines!(ax, muRange, mean_energies, std_energies, label=L"%$i", markersize=0.1,color = colors[i],linewidth = 2)
-        end
-        axislegend(ax, position=:lt, merge=true)
     end
 
-    rowsize!(fig.layout,1,Relative(0.2))
-    # colsize!(fig.layout,2,Relative(0.3))
+    DF = DataFrame(Sector=Sectors, mu=mus, Energy=Energy, StructureFactor=StructureFactor)
+    sort!(DF, [:Sector, :mu])
+    return DF
+end
 
-    SqFig[1,1] = axSQ = Axis(fig, xlabel=L"q_x", ylabel=L"q_y",aspect=1,xticks = PiTicks([0,pi]),yticks = PiTicks([0,pi]))
+##
+
+results_df = collect_results("../../Data/EnergyScaling_S1_L20")
+function plot_overview(results_df)
+    fig = Figure(size = (700, 500), fontsize = 22)
     
-    SqMat = mean(Sqs)[:,:,1]
-    SqErr = std(Sqs)[:,:,1]
+    EnergyFig = fig[2, 1:3] = GridLayout()
+    SqFig = fig[2, 4] = GridLayout()
+    toprow = fig[1, 1:4] = GridLayout()
+
+    sectors = unique(results_df.Sector)
+    colors = Makie.ColorSchemes.distinguishable_colors(length(sectors), lchoices = LinRange(0, 80, 100))
+
+    for (i, sector) in enumerate(sectors)
+        sector_data = results_df[results_df.Sector .== sector, :]
+        L = size(sector_data.StructureFactor[1],1)
+        conf = SW.get4x4PeriodicSpinConf(L,sector)
+        println(size(conf))
+        if sector == 6
+            conf .= 2SW.getStairCase(size(conf, 1)) # plot the staircase state, which is in the same sector
+        end
+        S = SW.stencilConfig(conf[1:8, 1:8], 1)
+
+        toprow[1, i] = ax_conf = Axis(fig, title=L"%$i"; SW.getConfigAxis(S)...,
+            yticklabelsvisible = false,
+            xticklabelsvisible = false,
+            spinecolors(colors[i])...,
+            spinewidth = 4
+        )
+        SW.plotSpinConfig!(ax_conf, S)
+        colgap!(toprow, 0)
+    end
+
+    EnergyFig[1, 1:8] = ax = with_theme(theme_SimpleTicks()) do
+        Axis(fig, xlabel=L"\mu", 
+            ylabel=L"E_0/N_{\text{sites}}"
+        )
+    end
+
+    for (i, sector) in enumerate(sectors)
+        sector_data = results_df[results_df.Sector .== sector, :]
+        L = size(sector_data.StructureFactor[1],1)
+
+        mus = sector_data.mu
+        energies = sector_data.Energy
+        mean_energies = mean.(energies) ./ L^2
+        std_energies = std.(energies) ./ L^2
+
+        errlines!(ax, mus, mean_energies, std_energies, label=L"%$i", markersize=0.1, color = colors[i], linewidth = 2)
+    end
+    axislegend(ax, position = :lt, merge = true)
+
+    rowsize!(fig.layout, 1, Relative(0.2))
+    colsize!(fig.layout, 4, Relative(0.3))
+
+    SqFig[1, 1] = axSQ = Axis(fig, xlabel=L"q_x", ylabel=L"q_y", aspect = 1, xticks = PiTicks([0, pi]), yticks = PiTicks([0, pi]))
     
-    Sq = SW.getSqCont(SqMat,cutoffEnd=0)
-    qx = qy = trueMomenta(-0.5pi,1.5pi,size(SqMat,1))
-    qs = Iterators.product(qx,qy)
-    heatmap!(axSQ,qx,qy, Sq.(qs))
+    let mu_plot = 0.08
+        muSim = results_df.mu[findfirst(>=(mu_plot), results_df.mu)]
+        resPl = only(filter(x->x.mu==muSim&&x.Sector == 1,results_df))
+        SqMat = dropmean(resPl.StructureFactor,dims=3)
+    
+        Sq = SW.getSqCont(SqMat, cutoffEnd = 0)
+        qx = qy = trueMomenta(-0.5pi, 1.5pi, size(SqMat, 1))
+        hm = heatmap!(axSQ, qx, qy, Sq)
+        Colorbar(SqFig[0,1], hm, label = L"\mathcal{S}(\mathbf{q})", vertical = false,ticks = SimpleTicks())
+    end
 
     fig
 end
 
-plot_overview(outfile_EnergyScaling,SqsGFMC)
+plot_overview(results_df)
